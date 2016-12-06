4a48eb91144eba539547541ced168294abb824e5
yangfan <13818556154@163.com>
2016-12-06 20:07:25 +0800

    添加网络工具

---
+++ BaiSiBuDeJie/BaiSiBuDeJie/Classes/Tools/HTTPRequestTool.h
@@ -0,0 +1,42 @@
+//
+//  HTTPRequestTool.h
+//  BaiSiBuDeJie
+//
+//  Created by YF on 16/10/19.
+//  Copyright © 2016年 YF. All rights reserved.
+//
+typedef NS_ENUM(NSUInteger,RequestType){
+    HTTPMethodGET,
+    HTTPMethodPOST,
+};
+/**
+ *  网络请求结果回调block
+ */
+typedef void (^CompletionBlock)(id result, NSError * error);
+
+#import <Foundation/Foundation.h>
+
+
+@interface HTTPRequestTool : AFHTTPSessionManager
+
+@property (nonatomic, copy) void(^complete)(id result,NSError * error);
+
+
+
++ (instancetype)defaultManager;
+
+/**
+ *  网络请求
+ */
+- (void)requestWithMethod:(RequestType)type param:(NSDictionary *)param urlString:(NSString *)urlString completion:(CompletionBlock)completion;
+
+/**
+ *  加载推荐订阅
+ */
+- (void)loadSubTag:(CompletionBlock)completion;
+/**
+ *  加载我的页面尾视图数据
+ */
+- (void)loadMeFooterData:(CompletionBlock)completion;
+
+@end
---
+++ BaiSiBuDeJie/BaiSiBuDeJie/Classes/Tools/HTTPRequestTool.m
@@ -0,0 +1,78 @@
+//
+//  HTTPRequestTool.m
+//  BaiSiBuDeJie
+//
+//  Created by YF on 16/10/19.
+//  Copyright © 2016年 YF. All rights reserved.
+//
+
+#import "HTTPRequestTool.h"
+
+@implementation HTTPRequestTool
+//单例
++ (instancetype)defaultManager
+{
+    static HTTPRequestTool * tool = nil;
+    static dispatch_once_t onceToken;
+    if (!tool) {
+        dispatch_once(&onceToken, ^{
+            tool = [[HTTPRequestTool alloc] init];
+        });
+    }
+    return tool;
+}
+//网络请求基本方法
+- (void)requestWithMethod:(RequestType)type param:(NSDictionary *)param urlString:(NSString *)urlString completion:(CompletionBlock)completion
+{
+    //如果是GET请求
+    if (type == HTTPMethodGET) {
+        [self GET:urlString parameters:param progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            completion(responseObject,nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            completion(nil,error);
+        }];
+    }
+    else {
+        [self POST:urlString parameters:param progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            completion(responseObject,nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            completion(nil,error);
+        }];
+    }
+}
+
+/**
+ *  加载推荐订阅
+ */
+- (void)loadSubTag:(CompletionBlock)completion
+{
+    NSMutableDictionary * param = [NSMutableDictionary dictionary];
+    param[@"a"] = @"tag_recommend";
+    param[@"action"] = @"sub";
+    param[@"c"] = @"topic";
+    [self requestWithMethod:HTTPMethodGET param:param urlString:subTagURL completion:^(id result, NSError *error) {
+        if (error == nil) {
+            completion(result,nil);
+        }
+        else {
+            completion(nil,error);
+        }
+    }];
+}
+
+- (void)loadMeFooterData:(CompletionBlock)completion
+{
+    NSMutableDictionary * param = [NSMutableDictionary dictionary];
+    param[@"a"] = @"square";
+    param[@"c"] = @"topic";
+    [self requestWithMethod:HTTPMethodGET param:param urlString:meFooterURL completion:^(id result, NSError *error) {
+        if (error == nil) {
+            completion(result,nil);
+        }
+        else {
+            completion(nil,error);
+        }
+    }];
+}
+
+@end
